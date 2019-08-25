320fb7ace0edc1e4afb48e065105991d6483d53f
Kazumasa Sakiyama <kazsakinotwen@gmail.com>
2019-08-25 22:38:42 +0900

    Upgrade to 2018.4

---
+++ Assets/MapCreator/Scripts/GameManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	Vector3 spawnPos = new Vector3 (0,20,0);
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+
+
+	public void Spawn() {
+		GameObject player
+			= PhotonNetwork.Instantiate("player", spawnPos,
+			                            Quaternion.identity, 0) as GameObject;
+		GameObject.Destroy (player.transform.Find ("Velox").gameObject);
+		player.transform.Find ("FirstPersonCharacter").gameObject.SetActive (true);
+		player.GetComponent<EditMap> ().enabled = true;
+		player.GetComponent<Movement> ().enabled = true;
+	}
+}
